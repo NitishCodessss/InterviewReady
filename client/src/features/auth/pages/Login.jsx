a2634a58a27e3df0a1bd488f6f2a01e34fbeb4bf
NitishCodessss <nitish304844@gmail.com>
2026-08-09 13:31:37 +0530

    register and login UI

--- client/src/features/auth/pages/Login.jsx
+++ client/src/features/auth/pages/Login.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import "../auth.form.scss"
+import {useNavigate, Link} from "react-router"
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+
   return (
-    <div>Login</div>
+    <>
+     <main>
+        <div className="leftBar">
+      
+        </div>
+        <div className="rightBar">
+          <div className="form-container">
+            <h1>Login</h1>
+            <form onSubmit= {handleSubmit}>
+              <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name= "email" placeholder="Enter email address"  />
+              </div>
+
+              <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name= "password" placeholder="Enter password"  />
+              </div>
+
+            <button className="button primary-button">Login</button>
+
+            </form>
+            <p>Don't have an account?
+              <span>
+                <Link to={"/register"}>register</Link>
+              </span>
+            </p>
+          </div>
+        </div>
+     </main>
+    </>
   )
 }
 

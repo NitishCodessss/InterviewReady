950f07a4e34b59038fe6b58c8b994cfe5847b50f
NitishCodessss <nitish304844@gmail.com>
2026-08-11 12:13:05 +0530

    add authHook in login

--- client/src/features/auth/hooks/useAuth.js
+++ client/src/features/auth/hooks/useAuth.js
@@ -1,32 +1,45 @@
-import { useContext } from "react"
-import { AuthContext } from "../auth.context.jsx"
-import { register, login, logout, getMe} from "../services/auth.api"
+import { useContext } from "react";
+import { AuthContext } from "../auth.context.jsx";
+import { register, login, logout, getMe } from "../services/auth.api";
 
 export const useAuth = () => {
-    const context = useContext(AuthContext);
-    const { user, setUser, loading, setLoading} = context
-    
-    const handleLogin = async ({email, password}) => {
-        setLoading(true);
-        const data = await login({email, password})
-        setUser(data.user)
-        setLoading(false)
+  const context = useContext(AuthContext);
+  const { user, setUser, loading, setLoading } = context;
+
+  const handleLogin = async ({ email, password }) => {
+    setLoading(true);
+    try {
+      const data = await login({ email, password });
+      setUser(data.user);
+    } catch (error) {
+      console.error("Unable to login: ", error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    const handleRegister = async ({username, email, password}) => {
-        setLoading(true);
-        const data = await register({username, email, password});
-        setUser(data.user);
-        setLoading(false);
+  const handleRegister = async ({ username, email, password }) => {
+    setLoading(true);
+    try {
+      const data = await register({ username, email, password });
+      setUser(data.user);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    const handleLogout = async () => {
-        setLoading(true);
-        const data = await logout();
-        setUser(null);
-        setLoading(false);
+  const handleLogout = async () => {
+    setLoading(true);
+    try {
+      const data = await logout();
+      setUser(null);
+    } catch (error) {
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return { user, loading, handleLogin, handleRegister, handleLogout};
-
-} 
+  return { user, loading, handleLogin, handleRegister, handleLogout };
+};
--- client/src/features/auth/pages/Login.jsx
+++ client/src/features/auth/pages/Login.jsx
@@ -1,48 +1,73 @@
-import React from 'react'
-import "../auth.form.scss"
-import {useNavigate, Link} from "react-router"
+import React, { useState } from "react";
+import "../auth.form.scss";
+import { useNavigate, Link } from "react-router";
+import { useAuth } from "../hooks/useAuth.js";
 
 const Login = () => {
   const navigate = useNavigate();
+  const {loading, handleLogin} = useAuth()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    handleLogin({ email, password });
+  };
+  if (loading) {
+    return (
+      <main>
+        <h1>Loading...</h1>
+      </main>
+    );
   }
-
 
   return (
     <>
-     <main>
-        <div className="leftBar">
-      
-        </div>
+      <main>
+        <div className="leftBar"></div>
         <div className="rightBar">
           <div className="form-container">
             <h1>Login</h1>
-            <form onSubmit= {handleSubmit}>
+            <form onSubmit={handleSubmit}>
               <div className="input-group">
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name= "email" placeholder="Enter email address"  />
+                <input
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter email address"
+                />
               </div>
 
               <div className="input-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" name= "password" placeholder="Enter password"  />
+                <input
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Enter password"
+                />
               </div>
 
-            <button className="button primary-button">Login</button>
-
+              <button className="button primary-button">Login</button>
             </form>
-            <p>Don't have an account?
+            <p>
+              Don't have an account?
               <span>
                 <Link to={"/register"}>Register</Link>
               </span>
             </p>
           </div>
         </div>
-     </main>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
